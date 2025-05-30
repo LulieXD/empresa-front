4c85d9c17f877bca0d66616e6158529089bc3f96
Mimiria <maria.fonseca30@escola.pr.gov.br>
2025-05-30 12:19:02 -0300

    Add files via upload

---
+++ doom/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=], initial-scale=1.0">
+    <title>Empresinha erriessi</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lexend&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+</head>
+<body>
+    
+<div class="content">
+        <!---Navegação--->
+        <nav class="navbar">
+            <A href="#Início" class="logo">DesignSwap ★</A>
+            <ul class="navlinks">
+                <li><A href="#Início">Início</A></li>
+                <li><A href="#Cardápio"> Cardápio</A></li>
+                <li><A href="#Promoções"> Promoções</A></li>
+                <li><A href="#Contato"> Contato</A></li>
+            </ul>
+        </nav>
+</div>
+
+
+
+
+</body>
+</html>
